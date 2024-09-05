a57081af2c427cdb8c44c8877aac0731e0d4064b
Atul Yadav <atul.yaduvanshi101@gmail.com>
2024-09-05 09:30:20 +0530

    Update 04_comparision.js

--- 01_Basics/04_comparision.js
+++ 01_Basics/04_comparision.js
@@ -1,1 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 == 3);
+// console.log(2 >= 4);
+// console.log(2 <= 2);
+// console.log(2 === 3);
+// console.log(2 != 3);
+
+
+// console.log(null > 0);  //false
+// console.log(null >= 0); //true //null converted as 0
+// console.log(null < 0);  //false
+// console.log(null <= 0); //true //null converted as 0
+// console.log(null == 0); //false
+
+// console.log(undefined >  0); //false
+// console.log(undefined >= 0); //false
+// console.log(undefined <  0); //false
+// console.log(undefined <= 0); //false
+// console.log(undefined == 0); //false
+
+
 
