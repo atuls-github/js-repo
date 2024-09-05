309400a9d0d708bd6ba4a01ef0f36b082850e423
Atul Yadav <atul.yaduvanshi101@gmail.com>
2024-09-05 04:35:37 +0000

    change in summery

--- 01_Basics/datatype_summery.js
+++ 01_Basics/datatype_summery.js
@@ -1,1 +1,8 @@
+//primitive datatype
+//  7 Types : String, Number, Boolean, null, undefined, symbol, BigInt
 
+const score = 100;
+const scoreValue = 100.3
+
+console.log(score);
+console.log("hitesh")
