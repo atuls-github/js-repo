c989c5f4e6bbc4bd47f552358fb9e7da31dcaa3a
Atul Yadav <atul.yaduvanshi101@gmail.com>
2024-09-05 05:10:51 +0000

    datatyep summery

--- 01_Basics/datatype_summery.js
+++ 01_Basics/datatype_summery.js
@@ -1,8 +1,36 @@
 //primitive datatype
+
 //  7 Types : String, Number, Boolean, null, undefined, symbol, BigInt
+//declaring variable
+const name = "rajesh" //string
+const score = 100;//number
+const scoreValue = 56;
+const isLoggedIn = false; //OR true //boolean
+const outSideTemp = null;
+let userEmail; //undefined value //declaration with "const" =>must be initialize
 
-const score = 100;
-const scoreValue = 100.3
+const Id = Symbol('123');//declare "symbol datatype"
+const anotherId = Symbol('123');//declare "symbol datatype"
 
-console.log(score);
-console.log("hitesh")
+console.log(Id == anotherId);//false //both are different
+
+
+
+//Reference Type (Non primitive type)
+   // Arrays, Objects, Functions
+
+const myHeros =["shaktiman","naageaj","doga"];//declare Array
+
+const myobj = {                //declare object
+    myName : "rajesh",
+    age : 18,
+}                           
+
+const myFunction = function(){        //declare function
+    console.log("function created")
+    }
+myFunction();  // Output: function created
+
+//find data type
+console.log(typeof /*variable*/ ) 
+
