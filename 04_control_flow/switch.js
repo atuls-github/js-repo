adc2d78bf2745548f1ea9a5f96a3f8346ca5161e
Atul Yadav <atul.yaduvanshi101@gmail.com>
2024-09-27 05:59:53 +0000

    switch and truthy/falsy values

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+//switch case syntax
+
+//switch (key){
+//     case value :
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log(january);
+        break;
+    case 1:
+        console.log(january);
+        break;
+    case 1:
+        console.log(january);
+            break;
+    default:
+        break;
+}
